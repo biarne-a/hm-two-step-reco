a3c382f73e4e157f9527e01feb21c7e4608751b1
a.biarnes <adrien.biarnes@dailymotion.com>
2022-08-23 16:06:49 +0200

    Introduced multi embeddings layer

---
+++ two-tower-cg/mutliple_embeddings_layer.py
@@ -0,0 +1,22 @@
+import math
+from typing import Dict
+import tensorflow as tf
+from tensorflow import keras
+
+
+class MultipleEmbeddingsLayer(keras.layers.Layer):
+    def __init__(self, lookups: Dict[str, tf.keras.layers.StringLookup]):
+        self._all_embeddings = {}
+        for categ_variable in lookups.keys():
+            lookup = lookups[categ_variable]
+            vocab_size = len(lookup.input_vocabulary) + 1
+            cat_var_emb_dim = int(3 * math.log(vocab_size, 2))
+            embedding_layer = tf.keras.layers.Embedding(vocab_size, cat_var_emb_dim)
+            self._all_embeddings[categ_variable] = embedding_layer
+
+    def call(self, inputs):
+        all_embeddings = []
+        for variable, embedding_layer in self._all_embeddings.items():
+            embeddings = embedding_layer(inputs[variable])
+            all_embeddings.append(embeddings)
+        return tf.concat(all_embeddings, axis=1)
--- two-tower-cg/train.py
+++ two-tower-cg/train.py
@@ -1,4 +1,3 @@
-import math
 import tensorflow as tf
 from typing import Dict
 from tensorflow import keras
@@ -7,6 +6,7 @@ from config import Config, Variables
 from custom_recall import CustomRecall
 from preprocess import PreprocessedHmData
 from basic_2_tower_model import Basic2TowerModel
+from mutliple_embeddings_layer import MultipleEmbeddingsLayer
 
 
 def build_tower_sub_model(vocab_size: int, embedding_dimension: int) -> tf.keras.Model:
@@ -19,16 +19,8 @@ def build_tower_sub_model(vocab_size: int, embedding_dimension: int) -> tf.keras
 
 def build_complex_sub_model(lookups: Dict[str, tf.keras.layers.StringLookup],
                             embedding_dimension: int):
-    all_embeddings = []
-    for categ_variable in lookups.keys():
-        lookup = lookups[categ_variable]
-        vocab_size = len(lookup.input_vocabulary) + 1
-        cat_var_emb_dim = int(3 * math.log(vocab_size, 2))
-        embedding = tf.keras.layers.Embedding(vocab_size, cat_var_emb_dim)
-        all_embeddings.append(embedding)
-    final_embedding = tf.concat(all_embeddings, axis=1)
     return tf.keras.Sequential([
-        final_embedding,
+        MultipleEmbeddingsLayer(lookups),
         tf.keras.layers.Dense(256, activation='relu'),
         tf.keras.layers.Dense(embedding_dimension, activation='relu')
     ])
