c4694f316e97b45374421f9cbece3317f560b8c7
a.biarnes <adrien.biarnes@dailymotion.com>
2022-09-04 12:17:43 +0200

    Use PR AUC as evaluation metric

--- mmoe-ranker/train.py
+++ mmoe-ranker/train.py
@@ -9,13 +9,16 @@ def run_training(data: PreprocessedHmData, config: Config):
     model = BasicRanker(data)
     model.compile(optimizer=tf.keras.optimizers.Adagrad(learning_rate=config.learning_rate),
                   loss=tf.keras.losses.BinaryCrossentropy(from_logits=False),
-                  metrics=[tf.keras.metrics.BinaryAccuracy()],
+                  metrics=[tf.keras.metrics.AUC(curve='PR')],
                   run_eagerly=False)
+
+    # model.fit(x=data.train_ds, epochs=1, steps_per_epoch=1, verbose=1)
+    # print(model.summary())
 
     return model.fit(x=data.train_ds,
                      epochs=config.nb_epochs,
-                     steps_per_epoch=data.nb_train_obs // config.batch_size,
-                     # steps_per_epoch=4000,
+                     # steps_per_epoch=data.nb_train_obs // config.batch_size,
+                     steps_per_epoch=4000,
                      validation_data=data.test_ds,
                      validation_steps=data.nb_test_obs // config.batch_size,
                      verbose=1)
