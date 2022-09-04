f74032c7412f5e59903c47662f5ec0dbd489aae4
a.biarnes <adrien.biarnes@dailymotion.com>
2022-09-04 14:47:58 +0200

    Try with class weights

--- mmoe-ranker/train.py
+++ mmoe-ranker/train.py
@@ -15,10 +15,19 @@ def run_training(data: PreprocessedHmData, config: Config):
     # model.fit(x=data.train_ds, epochs=1, steps_per_epoch=1, verbose=1)
     # print(model.summary())
 
+    neg = 7906360
+    pos = 93640
+    total = neg + pos
+    weight_for_0 = (1 / neg) * (total / 2.0)
+    weight_for_1 = (1 / pos) * (total / 2.0)
+
+    class_weight = {0: weight_for_0, 1: weight_for_1}
+
     return model.fit(x=data.train_ds,
                      epochs=config.nb_epochs,
                      # steps_per_epoch=data.nb_train_obs // config.batch_size,
                      steps_per_epoch=4000,
                      validation_data=data.test_ds,
                      validation_steps=data.nb_test_obs // config.batch_size,
+                     class_weight=class_weight,
                      verbose=1)
